b42655ee3461e4ffbce8a2a5ecfe58a95fdae284
LvisGlez <luisen.perezgonzalez@gmail.com>
2026-06-18 20:57:47 -0600

    agregando auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { authLimiter } = require('../middlewares/rateLimiter.middleware');
 const { sanitizeBody } = require('../middlewares/sanitize.middleware');
-const { proteger } = require('../middlewares/auth.middleware');
+const { proteger, restringirA } = require('../middlewares/auth.middleware');
 const { iniciarSesion, registrar, confirmarCorreo, cambiarContrasena, obtenerUsuarios } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
 router.post('/iniciar-sesion', authLimiter, sanitizeBody, iniciarSesion);
-router.post('/registrar', authLimiter, sanitizeBody, registrar);
+router.post('/registrar', proteger, restringirA('admin'), sanitizeBody, registrar);
 router.get('/confirmar/:token', confirmarCorreo);
 router.patch('/cambiar-contrasena', proteger, sanitizeBody, cambiarContrasena);
-router.get('/usuarios', proteger, obtenerUsuarios);
+router.get('/usuarios', proteger, restringirA('admin'), obtenerUsuarios);
 
 module.exports = router;
+
